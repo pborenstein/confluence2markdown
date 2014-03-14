ca2ffa7f6af068bda173ca029a644405b67569fb
Philip Borenstein <philip.borenstein@joyent.com>
2014-03-14 12:07:59 -0400

    First try at parsing through stuff. Fails miserably.

--- c2m.js
+++ c2m.js
@@ -5,12 +5,75 @@ var Tokenizer = require('tokenizer');
 
 function Confluence2Markdown() {
   stream.Transform.call(this, { objectMode: true });
+
+  this._inOCurly = false;
+  this._inLiteral = false;
+  this._inCode = false;
+  this._macro = '';
+  this._prevToken = { content: '', type: 'whitespace'};
+
+
+  this.mkLiteral = function (string) {
+                      return '`' + string + '`';
+                   }
+
+  this.handleMacro = function (string) {
+    if (string.match(/^code/) || string.match(/^noformat/)) {
+      this._inCode = ! this._inCode;
+      return '```';
+    }
+  }
+
 }
 util.inherits(Confluence2Markdown, stream.Transform);
 
-Confluence2Markdown.prototype._transform = function(chunk, encoding, done) {
-//  this.push(">>" + JSON.Confluence2Markdown(chunk) + "<<\n");
-  this.push(chunk.content);
+
+
+Confluence2Markdown.prototype._transform = function(token, encoding, done) {
+  var outText;
+
+  //this.push(">>" + JSON.stringify(token) + "<<\n");
+
+  outText = token.content;
+
+  if (token.type === 'ocurly') {
+    this._inOCurly = true;
+    if (this._prevToken.type === 'ocurly') {
+      this._inLiteral = true;
+    }
+    this._prevToken = token;
+    done();
+    return ;
+  }
+
+  if (this._inOCurly && token.type !== 'ccurly') {
+    this._macro += token.content;
+    this._prevToken = token;
+    done();
+    return ;
+  }
+
+  if (token.type === 'ccurly') {
+    if (this._inLiteral && this._prevToken.type === 'ccurly') {
+      outText = this.mkLiteral(this._macro);
+      this._inLiteral = false;
+      this._inOCurly = false;
+      this._macro = '';
+    } else if (this._inLiteral && this._prevToken.type !== 'ccurly') {
+      // swallow it
+      this._prevToken = token;
+      done();
+      return ;
+    } else if (! this._inLiteral) {
+      this._inOCurly = false;
+      outText = 'MACRO:' + this._macro + '<<';
+      this._macro = '';
+    }
+  }
+
+
+  this.push(outText);
+  this._prevToken = token;
   done();
 };
 c2m = new Confluence2Markdown();
@@ -18,8 +81,9 @@ c2m = new Confluence2Markdown();
 t = new Tokenizer();
 
 t.addRule(Tokenizer.whitespace);
-t.addRule(Tokenizer.word);
-t.addRule(Tokenizer.number);
+t.addRule(/^h[1-6]\.$/, 'heading');
+
+t.addRule(/^[\w\d\.,\-]+$/, 'word');
 
 t.addRule(/^\*$/, 'star');
 t.addRule(/^\_$/, 'under');
@@ -31,16 +95,17 @@ t.addRule(/^\}$/, 'ccurly');
 t.addRule(/^\[$/, 'obracket');
 t.addRule(/^\]$/, 'cbracket');
 t.addRule(/^[\(\)]$/, "parens")
-t.addRule(/^[@~;.:\?\^%<>=!&|+\-,\$\\\/]$/, "punct")
-t.addRule(/^h[1-6]\.$/, 'heading');
+t.addRule(/^[@~;:\?\^%<>=!&|+\$\\\/]$/, "punct")
 
 
 t.on('token', function (token, type) {
-             //  console.log('%s %s', token, type);
+
+               //console.log('>>%s %s<<', token, type);
               // t.push(token)
               });
 
 process.stdin.pipe(t)
              .pipe(c2m)
              .pipe(process.stdout)
+
 
