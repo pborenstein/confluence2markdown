886b2092365b127db133d5eb0df320c5ff7faf13
Philip Borenstein <philip.borenstein@joyent.com>
2014-03-15 01:16:02 -0400

    Move my tokenizer to its own module. Clean up

--- c2m.js
+++ c2m.js
@@ -1,90 +1,53 @@
 var stream    = require('stream');
 var util      = require('util');
-var Tokenizer = require('tokenizer');
+var Tokenizer = require('./lib/ConfluenceTokenizer.js');
 
 //  Some confluence macros are paired
 //    and some are not.
+//    paired macros of different types can nest inside each other
+//    verbatim macros are paired but cannot nest
 var macros = {
   'include'           : 'single',
   'toc'               : 'single',
   'children'          : 'single',
   'composition-setup' : 'single',
   'toggle-cloak'      : 'single',
-  'code'              : 'paired',
   'note'              : 'paired',
-  'cloak'             : 'paired'
+  'cloak'             : 'paired',
+  'writersnote'       : 'paired',
+  'code'              : 'verbatim',
+  'noformat'          : 'verbatim'
 }
 
-
+t = new Tokenizer();
 
 
 function Confluence2Markdown() {
   stream.Transform.call(this, { objectMode: true });
 
-  this._inOCurly = false;
-  this._inLiteral = false;
-  this._inCode = false;
+  this._state = {
+                  verbatim : false
+                };
+
   this._macro = '';
   this._prevToken = { content: '', type: 'whitespace'};
-
-
-  this.mkLiteral = function (string) {
-                      return '`' + string + '`';
-                   }
-
-  this.handleMacro = function (string) {
-    if (string.match(/^code/) || string.match(/^noformat/)) {
-      this._inCode = ! this._inCode;
-      return '```';
-    }
-  }
 
 }
 util.inherits(Confluence2Markdown, stream.Transform);
 
 
+//  Convert Confluence markup to Markdown one token at a time
 
 Confluence2Markdown.prototype._transform = function(token, encoding, done) {
   var outText;
+  var s = this._state; // make things easier to type
 
-  //this.push(">>" + JSON.stringify(token) + "<<\n");
-
-  outText = token.content;
-
-  if (token.type === 'ocurly') {
-    this._inOCurly = true;
-    if (this._prevToken.type === 'ocurly') {
-      this._inLiteral = true;
-    }
-    this._prevToken = token;
-    done();
-    return ;
+  if (s.verbatim && token.type !== s.verbatim) {
+    outText = token.content;
   }
 
-  if (this._inOCurly && token.type !== 'ccurly') {
-    this._macro += token.content;
-    this._prevToken = token;
-    done();
-    return ;
-  }
 
-  if (token.type === 'ccurly') {
-    if (this._inLiteral && this._prevToken.type === 'ccurly') {
-      outText = this.mkLiteral(this._macro);
-      this._inLiteral = false;
-      this._inOCurly = false;
-      this._macro = '';
-    } else if (this._inLiteral && this._prevToken.type !== 'ccurly') {
-      // swallow it
-      this._prevToken = token;
-      done();
-      return ;
-    } else if (! this._inLiteral) {
-      this._inOCurly = false;
-      outText = 'MACRO:' + this._macro + '<<';
-      this._macro = '';
-    }
-  }
+
 
 
   this.push(outText);
@@ -93,31 +56,6 @@ Confluence2Markdown.prototype._transform = function(token, encoding, done) {
 };
 c2m = new Confluence2Markdown();
 
-t = new Tokenizer();
-
-t.addRule(Tokenizer.whitespace);
-t.addRule(/^h[1-6]\.$/, 'heading');
-
-t.addRule(/^[\w\d\.,\-]+$/, 'word');
-
-t.addRule(/^\*$/, 'star');
-t.addRule(/^\_$/, 'under');
-t.addRule(/^\#$/, 'hash');
-t.addRule(/^\"$/, 'dquote');
-t.addRule(/^\'$/, 'squote');
-t.addRule(/^\{$/, 'ocurly');
-t.addRule(/^\}$/, 'ccurly');
-t.addRule(/^\[$/, 'obracket');
-t.addRule(/^\]$/, 'cbracket');
-t.addRule(/^[\(\)]$/, "parens")
-t.addRule(/^[@~;:\?\^%<>=!&|+\$\\\/]$/, "punct")
-
-
-t.on('token', function (token, type) {
-
-               //console.log('>>%s %s<<', token, type);
-              // t.push(token)
-              });
 
 process.stdin.pipe(t)
              .pipe(c2m)
--- tk.js
+++ tk.js
@@ -1,7 +1,8 @@
 var stream    = require('stream');
 var util      = require('util');
-var Tokenizer = require('tokenizer');
+var Tokenizer = require('./lib/ConfluenceTokenizer.js');
 
+var debug = true;
 
 function Stringify() {
   stream.Transform.call(this, { objectMode: true });
@@ -15,38 +16,9 @@ Stringify.prototype._transform = function(chunk, encoding, done) {
 };
 stringify = new Stringify();
 
-// https://github.com/Floby/node-tokenizer
-t = new Tokenizer();
 
+t = new Tokenizer(debug);
 
-// http://stackoverflow.com/questions/3469080/how-do-i-match-whitespace-but-not-newlines
-t.addRule(/^[^\S\r\n]+$/,           'whitespace'); // but not newlines
-t.addRule(/^(\n)+$/,                'newline');
-t.addRule(/^\\\\$/,                 'break');
-t.addRule(/^h[1-6]\.$/,             'heading');
-t.addRule(/^[-\w\d]+$/,             'word');
-t.addRule(/^{[\w-]+(:.*)?}$/,       'macro');
-t.addRule(/^{{[-\w\d=\/\\:. ]+}}$/, 'mono'); // spaces are allowed here
-t.addRule(/^\*$/,                   'star');
-t.addRule(/^\_$/,                   'under');
-t.addRule(/^\#$/,                   'hash');
-t.addRule(/^\"$/,                   'dquote');
-t.addRule(/^\'$/,                   'squote');
-t.addRule(/^\{\s$/,                 'ocurly');
-t.addRule(/^\}$/,                   'ccurly');
-t.addRule(/^{}$/,                   'nullobj'); // For handling null JSON objects in code
-t.addRule(/^\[$/,                   'obracket');
-t.addRule(/^\]$/,                   'cbracket');
-t.addRule(/^\($/,                   'oparen');
-t.addRule(/^\)$/,                   'cparen');
-t.addRule(/^[|]$/,                  'pipe');
-t.addRule(/^!$/,                    'bang');
-t.addRule(/^[@~;:\?\^%<>=&+\-\$\\\/\.,└─├]$/, "punct")
-
-t.on('token', function (token, type) {
-               console.log('>>%s<< %s', token, type);
-              // t.push(token)
-              });
 
 process.stdin.pipe(t)
              .pipe(stringify)
